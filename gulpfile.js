eb95aff8e2d26fd30d55e7f0dd7a202df03035f5
Unknown <scott@temboinc.com>
2018-07-17 23:16:28 -0700

    poop emoji

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch('_scss/**/*.scss', ['sass']);
     gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
